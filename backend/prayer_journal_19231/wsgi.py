798c3cf6d7df99fc80c1f789dbe0a6ac905b7212
Crowdbotics <team@crowdbotics.com>
2020-07-29 03:06:55 +0000

    Initial commit.

---
+++ backend/prayer_journal_19231/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for prayer_journal_19231 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prayer_journal_19231.settings')
+
+application = get_wsgi_application()
